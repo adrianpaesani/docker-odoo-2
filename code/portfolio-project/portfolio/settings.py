4f8a8e222b29524db04593bc41fff03063ad60a8
lucpn <lucpn@trobz.com>
2020-05-02 14:42:06 +0700

    [ADD] - add example django application

--- code/portfolio-project/portfolio/settings.py
+++ code/portfolio-project/portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "job",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'postgres',
+        "USER": 'postgres',
+        "PASSWORD": 'postgres',
+        "HOST": 'db',
+        "PORT": '5432',
     }
 }
 
@@ -86,20 +91,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": """django.contrib.auth.password_validation.
-UserAttributeSimilarityValidator""",
+        "NAME": 'django.contrib.auth.password_validation.' +
+        'UserAttributeSimilarityValidator',
     },
     {
-        "NAME": """django.contrib.auth.password_validation.
-MinimumLengthValidator""",
+        "NAME": 'django.contrib.auth.password_validation.' +
+        'MinimumLengthValidator',
     },
     {
-        "NAME": """django.contrib.auth.password_validation.
-CommonPasswordValidator""",
+        "NAME": 'django.contrib.auth.password_validation.' +
+        'CommonPasswordValidator',
     },
     {
-        "NAME": """django.contrib.auth.password_validation.
-NumericPasswordValidator""",
+        "NAME": 'django.contrib.auth.password_validation.' +
+        'NumericPasswordValidator',
     },
 ]
 
