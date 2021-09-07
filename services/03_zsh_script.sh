c59cb9122a2eb944303ddaeced2de8b23627a3f9
lucpn <lucpn@trobz.com>
2021-09-07 14:28:36 +0700

    [REF] - Refactoring source code

--- services/03_zsh_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo -u odoo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-if [[ "$USER_SHELL" == "/usr/bin/zsh" ]]; then
-    # info 'Download latest oh-my-zsh'
-    /bin/cp /scripts/zshrc "$OE_HOME/.zshrc"
-fi
